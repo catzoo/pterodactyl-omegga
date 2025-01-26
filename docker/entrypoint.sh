7b3e50c8c65d9bcae4925950d1a858fa8e0f1501
catzoo <34617626+catzoo@users.noreply.github.com>
2025-01-25 23:14:05 -0600

    Creating files

---
+++ docker/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd /home/container
+
+# Sets up the auth file with omegga if user.txt exists
+# For some reason omegga doesn't exit if you set -u and -p.
+# So request the user to kill the server and restart it.
+if [ -e user.txt ]
+then
+	echo "Found user.txt. Generating auth. After complete, please restart the server."
+	USER=$(sed '1q;d' user.txt)
+	PASS=$(sed '2q;d' user.txt)
+	rm user.txt
+	omegga auth -gl
+	omegga auth -u $USER -p $PASS
+fi
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Print Node.js Version
+node -v
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
